3ac94a44a0e58ebe831c07b7fe2aca72ef47acbc
Ben Scott <227292+BPScott@users.noreply.github.com>
2023-07-07 16:53:40 -0700

    Prettier v3 (#305)
    Minimum supported Prettier version is v3.0.0
    Minimum supported stylelint version is 15.8.0
    
    - Use `await import()` instead of require.
    - Use async (and now the only) versions of resolveConfig, getFileInfo
      and format
    - Tweak jest/eslint configs to account for prettier using esmodules
    - Ignore testing svelte files as prettier-plugin-svelte doesn't yet have
      a release that supports prettier v3

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,14 @@ module.exports = {
   plugins: ['node'],
   extends: ['plugin:node/recommended', 'plugin:prettier/recommended'],
   env: {node: true, jest: true},
+  parserOptions: {ecmaVersion: '2020'},
   root: true,
   rules: {
     'node/no-unpublished-require': ['error', {allowModules: ['stylelint']}],
     'node/no-missing-require': ['error', {allowModules: ['styled-components']}],
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {ignores: ['dynamicImport']},
+    ],
   },
 };
