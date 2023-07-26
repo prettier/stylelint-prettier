a1a8da2f7bacce4c5a5de8af8746bd26fda2ec41
Ben Scott <ben@reload.me.uk>
2023-07-26 11:07:14 -0700

    Consistent formatting in fixture config

--- test/fixtures/stylelint.config.js
+++ test/fixtures/stylelint.config.js
@@ -1,24 +1,30 @@
 module.exports = {
-  plugins: [`../..`],
+  plugins: ['../..'],
   rules: {
-    'prettier/prettier': [true, {singleQuote: true, trailingComma: 'all'}],
+    'prettier/prettier': [
+      true,
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      }
+    ],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["**/*.scss"],
-      "customSyntax": "postcss-scss",
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss',
     },
     {
-      "files": ["**/*.{js,jsx,tsx}"],
-      "customSyntax": "postcss-styled-syntax",
+      files: ['**/*.{js,jsx,tsx}'],
+      customSyntax: 'postcss-styled-syntax',
     },
     {
-      "files": ["**/*.{html,svelte,vue}"],
-      "customSyntax": "postcss-html",
+      files: ['**/*.{html,svelte,vue}'],
+      customSyntax: 'postcss-html',
     },
     {
-      "files": ["**/*.md"],
-      "customSyntax": "postcss-markdown",
+      files: ['**/*.md'],
+      customSyntax: 'postcss-markdown',
     },
   ]
 };
