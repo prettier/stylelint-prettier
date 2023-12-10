d81ff2d359610ee48aa510870040339acbab1a07
Ben Scott <ben@reload.me.uk>
2023-12-10 14:07:28 -0800

    Update eslint-plugin-n and strip-ansi
    We no longer support node 14/16 and we're pure-esm so we can use the
    latest versions of these packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,10 @@
 'use strict';
 
 module.exports = {
-  plugins: ['n'],
+  root: true,
   extends: [
     'eslint:recommended',
     'plugin:n/recommended',
     'plugin:prettier/recommended',
   ],
-  env: {node: true},
-  parserOptions: {ecmaVersion: '2020'},
-  root: true,
-  rules: {
-    'n/no-unpublished-require': ['error', {allowModules: ['stylelint']}],
-    'n/no-missing-require': ['error', {allowModules: ['styled-components']}],
-    'n/no-unsupported-features/es-syntax': [
-      'error',
-      {ignores: ['dynamicImport']},
-    ],
-  },
 };
