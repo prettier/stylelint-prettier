ee95d023d291c7d7de80e46aba15675b72b44e98
Ben Scott <ben@reload.me.uk>
2024-11-29 16:35:58 -0800

    Update eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,4 +10,16 @@ module.exports = {
   env: {
     node: true,
   },
+  overrides: [
+    {
+      files: ['test/*.test.js'],
+      rules: {
+        // Adjust tighten version to allow using `test` apis
+        'n/no-unsupported-features/node-builtins': [
+          'error',
+          {version: '^20.13'},
+        ],
+      },
+    },
+  ],
 };
